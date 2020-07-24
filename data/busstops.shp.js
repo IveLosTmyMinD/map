3123d15aa5ec6a1140d74de0e21050caf8014654
lost <ivelostmymind8@gmail.com>
2020-07-24 18:27:45 +0300

    Created map

---
+++ data/busstops.shp.js
@@ -0,0 +1,8 @@
+var busstops = {
+"type": "FeatureCollection",
+"name": "busstops",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "shop": 1, "routenumb": "14,8,19,12" }, "geometry": { "type": "Point", "coordinates": [ 38.43683, 55.802087 ] } }
+]
+}
